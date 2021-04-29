9519af463816e95a419d97a22590b0604aad4c8c
atban98 <62559545+atban98@users.noreply.github.com>
2021-04-29 20:30:16 +0200

    Changed the number of filter coefficients

--- RAIL-Audio-Transmitter-Receiver/app_task_init.h
+++ RAIL-Audio-Transmitter-Receiver/app_task_init.h
@@ -12,7 +12,7 @@
 #define PROPRIETARY_APP_TASK_STACK_SIZE   (1024 / sizeof(CPU_STK))
 #define FILTER_TASK_STACK_SIZE   (1024 / sizeof(CPU_STK))
 
-#define NUM_OF_COEFFS 42
+#define NUM_OF_COEFFS 64
 
 
 #endif /* APP_TASK_INIT_H_ */
